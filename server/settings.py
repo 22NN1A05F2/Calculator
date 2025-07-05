dd6af6a351d3a0cb3a0d5ae631b214d61bf2a746
SRUTHI MALLELA <161558474+22NN1A05F2@users.noreply.github.com>
2025-07-05 15:21:26 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ss#2&yo8kowoe@=o%u-&4fc@3k77e@h9)1t0!ft^1nld^ikx*h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
